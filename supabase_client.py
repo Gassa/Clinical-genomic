b903d6eb9a490b3574f74277c3e79ccc88e446f4
Moustapha Gassama <moustaphagassama19@gmail.com>
2026-04-12 02:18:54 -0400

    fix: utiliser clé anon legacy eyJ pour Supabase client

--- supabase_client.py
+++ supabase_client.py
@@ -2,7 +2,7 @@ import os
 from supabase import create_client, Client
 
 SUPABASE_URL = os.environ.get("SUPABASE_URL", "https://rfbayzcgceiyxdmxaoml.supabase.co")
-SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "sb_publishable_JnPiJvIUItmffh2Kh5FhAQ_KTwCWvKF")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmYmF5emNnY2VpeXhkbXhhb21sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MzExNDMsImV4cCI6MjA5MTAwNzE0M30.ydhV6XhQowpGWoarjVfDz25kIFLrXr2UxkEWRXiLz70")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
